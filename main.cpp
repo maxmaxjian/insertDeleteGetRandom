1ff077890ea9dcf7dc6c9213325a441e82b46dfc
maxmaxjian <jianw812@gmail.com>
2016-10-11 17:48:23 -0700

    first version

--- main.cpp
+++ main.cpp
@@ -1,17 +1,75 @@
+#include <iostream>
+#include <unordered_set>
+#include <cstdlib>
+#include <ctime>
 
 class RandomizedSet {
   public:
     RandomizedSet() {
+      std::srand(std::time(NULL));
     }
 
     bool insert(int val) {
+      bool found;
+      if (myset.find(val) == myset.end()) {
+	myset.insert(val);
+	found = true;
+      }
+      else
+	found = false;
+      return found;
     }
 
     bool remove(int val) {
+      bool success;
+      auto it = myset.find(val);
+      if (it != myset.end()) {
+	success = true;
+	myset.erase(it);
+      }
+      else
+	success = false;
+      return success;
     }
 
     int getRandom() {
+      size_t idx = rand()%myset.size();
+      auto it = myset.begin();
+      while (idx-- > 0)
+	it++;
+      return *it;
     }
 
-    
+  void print() {
+    for (auto it = myset.begin(); it != myset.end(); ++it)
+      std::cout << *it << " ";
+    std::cout << std::endl;
+  }
+
+private:
+  std::unordered_set<int> myset;
 };
+
+int main() {
+  RandomizedSet randomSet;
+
+  std::cout << std::boolalpha;
+  std::cout << randomSet.insert(1) << std::endl;
+  randomSet.print();
+
+  std::cout << randomSet.remove(2) << std::endl;
+  randomSet.print();
+
+  std::cout << randomSet.insert(2) << std::endl;
+  randomSet.print();
+
+  std::cout << randomSet.getRandom() << std::endl;
+
+  std::cout << randomSet.remove(1) << std::endl;
+  randomSet.print();
+
+  std::cout << randomSet.insert(2) << std::endl;
+  randomSet.print();
+
+  std::cout << randomSet.getRandom() << std::endl;
+}
